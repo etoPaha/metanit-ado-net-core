dcfaeedbd27766637679e902b5ea73bbea920de2
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2023-02-01 10:20:58 +1000

    Подключение к MS SQL Server

---
+++ MetanitAdoNetCore/N_2_2_ConnectToMSSQL/Examples.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace N_2_2_ConnectToMSSQL
+{
+    public class Examples
+    {
+        public static async Task N_1_SimpleConnection()
+        {
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=true;";
+
+            // Создаем подключение
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                // Открываем подключиние
+                await connection.OpenAsync();
+                
+                Console.WriteLine("Подключение открыто");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // если подключение открыто
+                if (connection.State == ConnectionState.Open)
+                {
+                    // закрываем подключение
+                    await connection.CloseAsync();
+
+                    Console.WriteLine("Подключение закрыто...");
+                }
+            }
+
+            Console.WriteLine("Программа завершила работу");
+            Console.Read();
+        }
+
+        public static async Task N_2_UsingConstructionForConnection()
+        {
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=true;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                Console.WriteLine("Подключение открыто");
+            }
+            
+            Console.WriteLine("Подключение закрыто...");
+            
+            Console.WriteLine("Программа завершила работу.");
+            Console.Read();
+        }
+    }
+}
---
+++ MetanitAdoNetCore/N_2_2_ConnectToMSSQL/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace N_2_2_ConnectToMSSQL
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // await Examples.N_1_SimpleConnection();
+            await Examples.N_2_UsingConstructionForConnection();
+        }
+    }
+}
